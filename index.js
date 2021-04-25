734e8966ca7c5ea77c883266ed6317d924e7f8fb
GerardoHuizarMartinez <lugehuizarma@ittepic.edu.mx>
2021-04-25 16:43:46 -0600

    Archivo principal

---
+++ index.js
@@ -0,0 +1,29 @@
+var strin = require("./cadena");
+
+let fra = strin.str("anita lava la tina");
+
+
+console.log(fra);
+
+
+//console.log(cadena("anita lava la tina"));
+
+
+//anita lava la tina qwertyuioplkjhgfdsazxcvbnm puto pito
+
+/*
+   ✅ Recibe una cadena                          
+
+   ✅ Verifica si es palíndromo
+
+   ✅ Cuenta el número de palabras en la cadena
+
+   ✅Cuenta el número de letras (sin espacios)
+
+   ✅Cuenta el número de vocales
+
+   ✅Cuenta el número de consonantes
+
+   ✅Imprime toda la información anterior en consola
+*/
+
